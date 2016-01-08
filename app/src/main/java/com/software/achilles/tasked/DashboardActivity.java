ce237d8b0af6176d7c9d905febc4ece011134e63
rsd-raul <rsd_raul@yahoo.es>
2016-01-08 15:51:09 +0100

    Clan FAB Library implemented and integrated, pending on scroll option.

--- app/src/main/java/com/software/achilles/tasked/DashboardActivity.java
+++ app/src/main/java/com/software/achilles/tasked/DashboardActivity.java
@@ -2,13 +2,16 @@ package com.software.achilles.tasked;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -19,16 +22,25 @@ public class DashboardActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab2);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
+    @Override
+    public void onBackPressed() {
+        FloatingActionMenu fam = (FloatingActionMenu) findViewById(R.id.menuFAB);
+
+        if(fam.isOpened())
+            fam.close(true);
+        else
+            super.onBackPressed();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
