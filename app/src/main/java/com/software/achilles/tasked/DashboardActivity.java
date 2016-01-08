b3b9253813f2b7fed6457c4c485462a960217372
rsd-raul <rsd_raul@yahoo.es>
2016-01-09 00:23:15 +0100

    Refactoring. Code isolation (FAB and FAM into Configurator.java and Styles.xml).

--- app/src/main/java/com/software/achilles/tasked/DashboardActivity.java
+++ app/src/main/java/com/software/achilles/tasked/DashboardActivity.java
@@ -1,21 +1,14 @@
 package com.software.achilles.tasked;
 
-import android.animation.ArgbEvaluator;
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+
 import com.github.clans.fab.FloatingActionMenu;
+import com.software.achilles.tasked.listeners.FloatingActionMenuConfigurator;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -26,64 +19,8 @@ public class DashboardActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab2);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
-        scrollFabMenuCustomization();
-    }
-
-    private void scrollFabMenuCustomization(){
-        final FloatingActionMenu fam = (FloatingActionMenu) findViewById(R.id.menuFAB);
-
-        // Change the background depending on the fabMenu Status
-        fam.setOnMenuToggleListener(new FloatingActionMenu.OnMenuToggleListener() {
-            Context context = getApplicationContext();
-            int fromColor = ContextCompat.getColor(context, R.color.transparent);
-            int toColor = ContextCompat.getColor(context, R.color.background);
-
-            final ObjectAnimator backgroundColorAnimator = ObjectAnimator.ofObject(
-                                fam, "backgroundColor", new ArgbEvaluator(), fromColor, toColor);
-
-            @Override
-            public void onMenuToggle(boolean opened) {
-                if (opened)
-                    backgroundColorAnimator.start();
-                else
-                    backgroundColorAnimator.reverse();
-            }
-        });
-
-        //
-        fam.setClosedOnTouchOutside(true);
-
-        Animation fab_slide_down = AnimationUtils.loadAnimation(this, R.anim.fab_slide_down);
-        fab_slide_down.setInterpolator(new AccelerateInterpolator());
-
-        Animation fab_slide_up = AnimationUtils.loadAnimation(this, R.anim.fab_slide_up);
-        fab_slide_up.setInterpolator(new AccelerateInterpolator());
-
-        fam.setMenuButtonHideAnimation(fab_slide_down);
-        fam.setMenuButtonShowAnimation(fab_slide_up);
-
-        NestedScrollView nestedScrollView = (NestedScrollView) findViewById(R.id.scrollView);
-        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-            @Override
-            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY,
-                                       int oldScrollX, int oldScrollY) {
-                int direction = scrollY-oldScrollY;
-
-                if (direction > 0)
-                    fam.hideMenu(true);
-                else
-                    fam.showMenu(true);
-            }
-        });
+        // Configure the fab menu and its children.
+        new FloatingActionMenuConfigurator(this);
     }
 
     @Override
@@ -98,7 +35,6 @@ public class DashboardActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_dashboard, menu);
         return true;
     }
---
+++ app/src/main/java/com/software/achilles/tasked/listeners/FloatingActionMenuConfigurator.java
@@ -0,0 +1,109 @@
+package com.software.achilles.tasked.listeners;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.software.achilles.tasked.R;
+
+public class FloatingActionMenuConfigurator {
+
+    private Activity activity;
+    private final FloatingActionMenu fam;
+
+    public FloatingActionMenuConfigurator(Activity activity) {
+        this.activity = activity;
+        fam = (FloatingActionMenu) activity.findViewById(R.id.menuFAB);
+
+        configureMenu();
+        setOnScrollReaction();
+
+        configureChildren();
+    }
+
+    private void configureMenu(){
+
+        // Change the background depending on the fabMenu Status
+        fam.setOnMenuToggleListener(new FloatingActionMenu.OnMenuToggleListener() {
+            Context context = activity.getApplicationContext();
+            int fromColor = ContextCompat.getColor(context, R.color.transparent);
+            int toColor = ContextCompat.getColor(context, R.color.background);
+
+            final ObjectAnimator backgroundColorAnimator = ObjectAnimator.ofObject( fam,
+                    "backgroundColor", new ArgbEvaluator(), fromColor, toColor).setDuration(100);
+
+            @Override
+            public void onMenuToggle(boolean opened) {
+                if (opened)
+                    backgroundColorAnimator.start();
+                else
+                    backgroundColorAnimator.reverse();
+            }
+        });
+
+        // On click outside
+        fam.setClosedOnTouchOutside(true);
+    }
+
+    private void setOnScrollReaction(){
+
+        // prepare animations
+        Animation fab_slide_down = AnimationUtils.loadAnimation(activity, R.anim.fab_slide_down);
+        fab_slide_down.setInterpolator(new AccelerateInterpolator());
+
+        Animation fab_slide_up = AnimationUtils.loadAnimation(activity, R.anim.fab_slide_up);
+        fab_slide_up.setInterpolator(new AccelerateInterpolator());
+
+        // Set animations
+        fam.setMenuButtonHideAnimation(fab_slide_down);
+        fam.setMenuButtonShowAnimation(fab_slide_up);
+
+        // Control the behaviour when scrolling
+        NestedScrollView scrollView = (NestedScrollView) activity.findViewById(R.id.scrollView);
+        scrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY,
+                                                           int oldScrollX, int oldScrollY) {
+                if (scrollY-oldScrollY > 0)
+                    fam.hideMenu(true);
+                else
+                    fam.showMenu(true);
+            }
+        });
+    }
+
+    private void configureChildren(){
+        FloatingActionButton share = (FloatingActionButton) activity.findViewById(R.id.shareListFAB);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        FloatingActionButton add = (FloatingActionButton) activity.findViewById(R.id.addFAB);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        FloatingActionButton addMultiple = (FloatingActionButton) activity.findViewById(R.id.addMultipleFAB);
+        addMultiple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
