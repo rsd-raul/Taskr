306026acf6cf90d3c75881861cd4369bd90c51b2
rsd-raul <rsd_raul@yahoo.es>
2016-03-06 12:30:49 +0000

    PreferencesHelper completed. Setting to select between Dashboard and Glance.

---
+++ app/src/main/java/com/software/achilles/tasked/model/helpers/PreferencesHelper.java
@@ -0,0 +1,74 @@
+package com.software.achilles.tasked.model.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferencesHelper {
+
+    // --------------------------- Values ----------------------------
+
+    public static class Keys{
+        public static final String MAIN = "dash_or_glance";
+        public static final String ADAPT_COLOR = "adaptColor";
+        public static final String NOTIF_SOUND = "notificationTone";
+        public static final String NOTIF_VIBRATION = "notificationVibration";
+        public static final String NOTIF_LED = "notificationLed";
+        public static final String SYNC_MODE="syncMode";
+        public static final String FIRST_DAY="firstDay";
+    }
+
+    public static class FileName{
+        // For preference between Dashboard and Glance
+        public static final String SETTINGS = "settings";
+        public static final String CONFIGURATIONS="configurations";
+    }
+
+    // --------------------------- Getters ---------------------------
+
+    public static int getSharedPreferencesInt(Context context, String key, int defaultValue) {
+        return getSharedPreferences(context).getInt(key, defaultValue);
+    }
+
+    public static String getSharedPreferencesString(Context context, String key, String defaultValue) {
+        return getSharedPreferences(context).getString(key, defaultValue);
+    }
+
+    public static boolean getSharedPreferencesBoolean(Context context, String key, boolean defaultValue) {
+        return getSharedPreferences(context).getBoolean(key, defaultValue);
+    }
+
+    public static float getSharedPreferencesFloat(Context context, String key, float defaultValue) {
+        return getSharedPreferences(context).getFloat(key, defaultValue);
+    }
+
+    public static long getSharedPreferencesLong(Context context, String key, long defaultValue){
+        return getSharedPreferences(context).getLong(key, defaultValue);
+    }
+
+    // --------------------------- Setters ---------------------------
+
+    public static void setPreferenceInteger(Context context, String key, Integer value){
+        getSharedPreferences_Editor(context).putInt(key, value).apply();
+    }
+    public static void setPreferenceString(Context context, String key, String value) {
+        getSharedPreferences_Editor(context).putString(key, value).apply();
+    }
+    public static void setPreferenceBoolean(Context context, String key, Boolean value){
+        getSharedPreferences_Editor(context).putBoolean(key, value).apply();
+    }
+    public static void setPreferenceFloat(Context context, String key, Float value){
+        getSharedPreferences_Editor(context).putFloat(key, value).apply();
+    }
+    public static void setPreferenceLong(Context context, String key, Long value){
+        getSharedPreferences_Editor(context).putLong(key, value).apply();
+    }
+
+    // -------------------------- Auxiliary --------------------------
+
+    private static SharedPreferences getSharedPreferences(Context context){
+        return context.getSharedPreferences(FileName.SETTINGS, Context.MODE_PRIVATE);
+    }
+    private static SharedPreferences.Editor getSharedPreferences_Editor(Context context){
+        return getSharedPreferences(context).edit();
+    }
+}
