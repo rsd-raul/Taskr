4d3f04ebe34872b6f446327398ccf9cc2b67b2e9
rsd-raul <rsd_raul@yahoo.es>
2016-03-08 02:52:33 +0000

    New listeners/color for buttons on TaskCreationFragment.

--- app/src/main/java/com/software/achilles/tasked/view/fragments/TaskCreationFragment.java
+++ app/src/main/java/com/software/achilles/tasked/view/fragments/TaskCreationFragment.java
@@ -2,16 +2,24 @@ package com.software.achilles.tasked.view.fragments;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -34,6 +42,8 @@ public class TaskCreationFragment extends Fragment {
 
     private Spinner mSpinner;
     private FloatingActionButton mFabSaveAndVoice;
+    private ImageButton mDescription, mReminder, mLocation, mLabels;
+    private CheckBox mFavourite;
 
     // ------------------------- Constructor -------------------------
 
@@ -58,55 +68,122 @@ public class TaskCreationFragment extends Fragment {
                 mPresenter.setupLayout();
             }
         });
-
-//        // Time button initializing
-//        Button taskTimeBT = (Button) getView().findViewById(R.id.time);
-//        taskTimeBT.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                showTimePickerDialog();
-//            }
-//        });
-
-//        // On lollipop assign a margin to separate the view from the ActionBar
-//        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.LOLLIPOP)
-//            return;
-//
-//        RelativeLayout relative = (RelativeLayout) getActivity().findViewById(R.id.relative);
-//        MarginLayoutParams params = (MarginLayoutParams) relative.getLayoutParams();
-//        params.setMargins(0, 50, 0, 0);
-
     }
 
     public void setupLayout(List<String> taskListNames){
         mFabSaveAndVoice = (FloatingActionButton) mMainActivity.findViewById(R.id.saveAndVoiceFAB);
         mSpinner = (Spinner) mMainActivity.findViewById(R.id.spinner_task_list);
+        mDescription = (ImageButton) mMainActivity.findViewById(R.id.button_description);
+        mReminder = (ImageButton) mMainActivity.findViewById(R.id.button_time);
+        mLocation = (ImageButton) mMainActivity.findViewById(R.id.button_location);
+        mLabels = (ImageButton) mMainActivity.findViewById(R.id.button_label);
+        mFavourite = (CheckBox) mMainActivity.findViewById(R.id.checkbox_favourite);
+
+        setupColors();
+        setupListeners();
 
         // Setup the fab and its listeners
-        setupFab();
+        setupSaveOrVoice(false);
 
         // Populate Spinner
         mSpinner.setAdapter(new ArrayAdapter<>(this.getContext(),
                 android.R.layout.simple_list_item_1, taskListNames));
     }
 
-    private void setupFab(){
-        mFabSaveAndVoice.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                // Save and reset fields (except spinner)
-                Toast.makeText(mMainActivity,"Save and reset",Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-        mFabSaveAndVoice.setOnClickListener(new View.OnClickListener() {
+    private void setupColors(){
+        int color = ContextCompat.getColor(mMainActivity, R.color.secondaryText);
+        PorterDuffColorFilter filter = new PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY);
+
+        mDescription.setColorFilter(filter);
+        mReminder.setColorFilter(filter);
+        mLocation.setColorFilter(filter);
+        mLabels.setColorFilter(filter);
+        mFavourite.setHintTextColor(color);
+        mFavourite.setHighlightColor(color);
+    }
+
+    private void setupListeners(){
+        View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Save and go back to the Dashboard/Glance
-                Toast.makeText(mMainActivity,"Save and go back",Toast.LENGTH_SHORT).show();
-            }
-        });
+                switch (v.getId()){
+                    case R.id.button_description:
+                        Toast.makeText(mMainActivity, "Button id: " + v.getId(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.button_time:
+                        Toast.makeText(mMainActivity, "Button id: " + v.getId(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.button_location:
+                        Toast.makeText(mMainActivity, "Button id: " + v.getId(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.button_label:
+                        Toast.makeText(mMainActivity, "Button id: " + v.getId(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.checkbox_favourite:
+                        Toast.makeText(mMainActivity, "Button id: " + v.getId(), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }};
+
+        mDescription.setOnClickListener(listener);
+        mReminder.setOnClickListener(listener);
+        mLocation.setOnClickListener(listener);
+        mLabels.setOnClickListener(listener);
+        mFavourite.setOnClickListener(listener);
     }
+
+    private void setupSaveOrVoice(boolean save){
+        View.OnLongClickListener longClickListener;
+        View.OnClickListener clickListener;
+        int dra, col;
+
+        if(save) {
+            dra = R.drawable.ic_save;
+            col = R.color.colorSuccess;
+
+            // Define listeners
+            longClickListener = new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // Save and reset fields (except spinner)
+                    Toast.makeText(mMainActivity, "Save and reset", Toast.LENGTH_SHORT).show();
+                    return false;
+                }};
+            clickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Save and go back to the Dashboard/Glance
+                    Toast.makeText(mMainActivity, "Save and go back", Toast.LENGTH_SHORT).show();
+                }};
+
+        }else{
+            dra = R.drawable.ic_voice_microphone;
+            col = R.color.colorAccent;
+
+            // Define listeners
+            longClickListener = null;
+            clickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Launch the microphone and write in the title field
+                    Toast.makeText(mMainActivity, "Micro to title", Toast.LENGTH_SHORT).show();
+                }};
+        }
+
+        mFabSaveAndVoice.setImageDrawable(ContextCompat.getDrawable(mMainActivity, dra));
+        mFabSaveAndVoice.setBackgroundTintList(ContextCompat.getColorStateList(mMainActivity, col));
+
+        mFabSaveAndVoice.setOnClickListener(clickListener);
+        mFabSaveAndVoice.setOnLongClickListener(longClickListener);
+
+        ScaleAnimation expandAnimation = new ScaleAnimation(0f, 1f, 0f, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        expandAnimation.setDuration(150);
+        expandAnimation.setInterpolator(new AccelerateInterpolator());
+
+        mFabSaveAndVoice.startAnimation(expandAnimation);
+    }
+    
 
     @Override
     public void onDestroy() {
